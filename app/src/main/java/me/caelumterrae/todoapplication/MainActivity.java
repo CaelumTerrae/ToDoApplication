cc939b05a1342d326ad2e2bafc6984e7feef55a9
Gavin Lifrieri <gavinlifrieri@fb.com>
2018-06-27 16:08:53 -0700

    edit features

--- app/src/main/java/me/caelumterrae/todoapplication/MainActivity.java
+++ app/src/main/java/me/caelumterrae/todoapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package me.caelumterrae.todoapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -19,9 +20,35 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    // a numeric code to identify the edit activity
+    public static final int EDIT_REQUEST_CODE = 20;
+    // keys used for passing data between activities
+    public static final String ITEM_TEXT = "itemText";
+    public static final String ITEM_POSITION = "itemPosition";
+
     ArrayList<String> items;
     ArrayAdapter<String> itemsAdapter;
     ListView lvItems;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // EDIT_REQUEST_CODE defined with constants
+        if (resultCode == RESULT_OK && requestCode == EDIT_REQUEST_CODE) {
+            // extract updated item value from result extras
+            String updatedItem = data.getExtras().getString(ITEM_TEXT);
+            // get the position of the item which was edited
+            int position = data.getExtras().getInt(ITEM_POSITION, 0);
+            // update the model with the new item text at the edited position
+            items.set(position, updatedItem);
+            // notify the adapter the model changed
+            itemsAdapter.notifyDataSetChanged();
+            // Store the updated items back to disk
+            writeItems();
+            // notify the user the operation completed OK
+            Toast.makeText(this, "Item updated", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +89,18 @@ public class MainActivity extends AppCompatActivity {
                 writeItems();
                 Log.i("Mainactivity", "Removed item " + position);
                 return true;
+            }
+        });
+
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent i = new Intent(MainActivity.this, EditItemActivity.class);
+
+                i.putExtra(ITEM_POSITION, position);
+                i.putExtra(ITEM_TEXT, items.get(position));
+                startActivityForResult(i, EDIT_REQUEST_CODE);
+
             }
         });
     }
